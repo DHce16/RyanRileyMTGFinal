a5ec6f9ba036aeca96f2831b353991c3c6efa29c
Dhce16 <ryan.riley@enhanceit.us>
2022-08-07 16:04:00 -0400

    first view shows

--- app/src/main/java/com/example/ryanrileymtgfinal/view/fragment/SearchFragment.kt
+++ app/src/main/java/com/example/ryanrileymtgfinal/view/fragment/SearchFragment.kt
@@ -1,6 +1,30 @@
 package com.example.ryanrileyfinalproject.view.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.ryanrileymtgfinal.databinding.FragmentSearchBinding
 
 class SearchFragment: Fragment() {
+    private var _binding: FragmentSearchBinding? = null
+    private val binding: FragmentSearchBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSearchBinding.inflate(layoutInflater)
+
+        binding.btnStartSearch.setOnClickListener {
+            getBoosters()
+        }
+        return binding.root
+    }
+
+    fun getBoosters() {
+
+    }
 }
